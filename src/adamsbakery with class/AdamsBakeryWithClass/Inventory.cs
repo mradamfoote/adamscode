791f6f04b0ce745676443106ff77668b629cb435
mradamfoote <mradamfoote@gmail.com>
2018-06-04 17:33:53 -0700

    updated both class

--- src/adamsbakery with class/AdamsBakeryWithClass/Inventory.cs
+++ src/adamsbakery with class/AdamsBakeryWithClass/Inventory.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace AdamsBakeryWithClass
 {
+    public struct BakeryItem
+    {
+        public string type;
+        public double salePrice;
+        public int quantity;
+        public double cost;
+        public double totalCost;
+        public double totalPrice;
+        public int itemNumber;
+    }
+
     static class Inventory
     {
         static int index;
@@ -15,14 +26,30 @@ namespace AdamsBakeryWithClass
             index = 0;
         }
 
+        static void IsEmpty()
+        {
+            Console.WriteLine("Inventory is Empty.");
+        }
+
          
-        static void ListInventory(Program.Bakery bakeryItem)
+        static void ListInventoryItem(BakeryItem bakeryItem)
         {
             //Console.WriteLine("\nAdam's Bakery Inventory");
             //Console.WriteLine("-----------------------");
+           // Console.WriteLine("Index     Item                Quantity     Cost     Sales Price     Total Cost     Total Sales Price");
+           // Console.WriteLine("----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("{0,-10}{1, -20}{2, 8}{3, 9:c}{4, 16:c}{5, 15:c}{6, 22:c}", bakeryItem.itemNumber, bakeryItem.type, bakeryItem.quantity, bakeryItem.cost, bakeryItem.salePrice, bakeryItem.totalCost, bakeryItem.totalPrice);
+        }
+        static void ListInventroy(BakeryItem[] bakery)
+        {
+            Console.WriteLine("\nAdam's Bakery Inventory");
+            Console.WriteLine("-----------------------");
             Console.WriteLine("Index     Item                Quantity     Cost     Sales Price     Total Cost     Total Sales Price");
             Console.WriteLine("----------------------------------------------------------------------------------------------------");
-            Console.WriteLine("{0,-10}{1, -20}{2, 8}{3, 9:c}{4, 16:c}{5, 15:c}{6, 22:c}", bakeryItem.itemNumber, bakeryItem.type, bakeryItem.quantity, bakeryItem.cost, bakeryItem.salePrice, bakeryItem.totalCost, bakeryItem.totalPrice);
+            foreach(var item in bakery)
+            {
+                ListInventoryItem(item);
+            }
         }
     }
 }
